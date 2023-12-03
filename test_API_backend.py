06d1638b799b7ed083d316390512d861a19cf072
Or-Shamir <152789047+Or-Shamir@users.noreply.github.com>
2023-12-03 19:24:09 +0200

    Add files via upload

---
+++ test_API_backend.py
@@ -0,0 +1,146 @@
+"""
+For each test you develop, you should add documentation (test steps, expected
+results, actual results).
+
+
+Test steps:
+1. I want to start by verifying that the DB is
+   Indeed empty. I will start in a 'get_all_api' request to check it.
+   If the db is not empty I will use the 'delete_api' method to delete all users in it.
+
+   Expected results : None/empty []
+   Actual results : None/empty  []
+
+
+2. If the DB is indeed empty I will create some users,
+   At first 1 user using the 'Post' method, and immediately after that the 'Get' method
+   to validate the action. If I see it works properly I will continue creating more users.
+
+   data: {"name": "Wall-E46765", "id": "666" }
+   Expected results : response 200, {"name": "Wall-E46765", "id": "666" }
+   Actual results : response 500, internal server error
+
+
+3. If my user was created successfully, Try to 'post' multiple users at ones, in a list of jsons and validate the
+   request failure and the correct response.
+
+   data: [{"name": "Omri", "id": "2"}, {"name": "Gaia", "id": "3"}, {"name": "Alon", "id": "4"}, {"name": "Wall-E", "id": "5"}]
+   Expected results : should receive 400 response-bad request / 401-not found etc.
+   Actual results :  500 Internal Server Error and my list of users has been added to the db.
+
+
+4. Try to 'post' duplicate users - same name and id , and then try to retrieve them by 'get' and examine the response.
+
+   data: {"name": "Wall-E46765", "id": "666" }
+   Expected results : 409 response-conflict in records
+   Actual results : response 500, internal server error
+
+
+5. I will continue using the 'Put' method to verify changes in the DB users,
+   and validate their changes using the 'Get' method.
+
+   data: {"name": "Yair", "id": "666"}
+   Expected results : should receive response of 200 and the updated json: {"name": "Yair", "id": "666"}
+   Actual results :  response 500, internal server error
+
+
+6. I will try to delete all users by using the 'Delete' method, and validate that using the 'Get' method.
+
+   "http://localhost:5000/users" - without mentioning a specific user-id
+   Expected results : should receive 400 response-bad request / 401-not found etc.
+   Actual results : response 500, internal server error
+
+
+7. After I finished the 'flow' tests, I want to test and validate the methods instructions of json formats.
+   Try to send all the methods with the opposite structure - instead of {name: str, id: str} --> send {id: str, name: str}
+
+   Expected results : should receive 400 response-bad request
+   Actual results : 200 (in the methods that didn't get 500)
+
+
+8. try to post semi-duplicated users- same name OR same id.
+   data: {"name": "Yair", "id": "898"} /  {"name": "Michal", "id": "898"}
+   Expected results : should receive response of 200 and the updated json: {"name": "Yair", "id": "898"} /  {"name": "Michal", "id": "898"}
+   (there is no Unique specification in the documentation)
+   Actual results :  response 500, internal server error
+
+
+
+ Bugs:
+
+ 1. In the 'Get user' & 'Post user' method, I supposed to receive a json response
+  with a { name: str, id: str} structure, instead I received a json with the following structure-
+  {id: str, name: str}.
+
+ 2. In the 'Post user' method I was able to create a new user with the opposite json format
+    instead of {name: str, id: str} --> send {id: str, name: str}, it is not supposed to succeed.
+
+ 3.  when I try to 'Post' a list of users I supposed to receive a 400 code- bad request because there isn't such method,
+    but instead I receive a 500 code, and my list of users does in fact being added to the db.
+
+ 4. 'Post user' allows you to create new users with the same name and id multiple times and due to that the
+
+ 5. 'Put' & 'Delete' not working properly, when I try to send the requests, in both json structures (regular and opposite),
+    I receive a '500' response-server error.
+
+"""
+
+import pytest
+import requests
+
+api_url = "http://localhost:5000/users"
+
+
+@pytest.fixture(scope="module")
+def validate_db_empty():
+    response_empty = requests.get("http://localhost:5000/users")
+    assert response_empty.text == ""
+
+
+@pytest.fixture
+def post_api():
+    # Post 1 user #
+    data = {"name": "Wall-E46765","id": "666"}
+    response_post = requests.post(api_url, json=data)
+    print(response_post.text)
+    print(response_post.status_code)
+    return response_post
+
+
+@pytest.fixture
+def get_api():
+    # Get 1 user #
+    response_get = requests.get("http://localhost:5000/users"+"/Wall-E46765")
+    print(response_get.text)
+    return response_get.text
+
+
+@pytest.fixture
+def get_all_api():
+    # Get all users #
+    response_get_all_users = requests.get("http://localhost:5000/users")
+    print(response_get_all_users.text)
+    return response_get_all_users.text
+
+
+@pytest.fixture
+def put_api():
+    # Put - edit user #
+    # trying to change the name of the user
+    data = {"name": "Yair", "id": "666"}
+    response_put = requests.put("http://localhost:5000/users/666", json=data)
+    print(response_put.text)
+    return response_put.text
+
+
+@pytest.fixture
+def delete_api():
+    # Delete a user #
+    response_delete = requests.delete("http://localhost:5000/users/666")
+    print(response_delete.text)
+    return response_delete.text
+
+
+
+
+
---
+++ test_links.py
@@ -0,0 +1,73 @@
+"""
+Test steps:
+1. Open a new browser window.
+2. Navigating to the required url : https://www.90min.com.
+3. Maximize the window in order to see the links in the header menu.
+4. Locate the headers menu element.
+5. Find all the links texts within the header and insert them into a 'header_links' list.
+6. Create a 'links_to_assert' list with all the required links texts.
+7. Iterate through both lists and validate the links texts from the 'links_to_assert' list
+   appears in the 'header_links' list.
+
+
+Expected results:
+1-4. None
+5. header_links = ['News', 'Transfer News', 'Premier League', 'European Leagues', 'Champions League', 'Women', 'Features']
+6. links_to_assert = ["Premier League", "Champions League", "FanVoice", "The Switch", "EFL",
+                     "La Liga", "Serie A", "More", "Transfers", ]
+7. Assertion error will fail the test in the first required link text that wasn't in the 'header_links' list.
+
+
+Actual results:
+1-4. None
+5. header_links = ['News', 'Transfer News', 'Premier League', 'European Leagues', 'Champions League', 'Women', 'Features']
+6. links_to_assert = ["Premier League", "Champions League", "FanVoice", "The Switch", "EFL",
+                     "La Liga", "Serie A", "More", "Transfers", ]
+7.   AssertionError: 'FanVoice' link is not displayed in the header menu
+          assert 'FanVoice' in ['News', 'Transfer News', 'Premier League', 'European Leagues', 'Champions League', 'Women', ...]
+
+"""
+
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+@pytest.fixture(scope="module")
+def driver_setup():
+    driver = webdriver.Chrome()
+    yield driver
+
+
+def test_check_header_links(driver_setup):
+    driver = driver_setup
+    # Navigating to the required url
+    driver.get('https://www.90min.com')
+    # Maximize the window in order to see the links
+    driver.maximize_window()
+
+    # Locate the headers menu element
+    headers_menu = driver.find_element(By.XPATH, '//ul[@class="fixedUl_ka6l4t"]')
+
+    # Find all the links texts within the header
+    header_links = headers_menu.text.split('\n')
+
+    # These are the links texts that are required to search
+    links_to_assert = ["Premier League", "Champions League", "FanVoice", "The Switch", "EFL",
+                     "La Liga", "Serie A", "More", "Transfers"]
+
+    # Iterate through both lists and validate the links texts from the 'links to assert' list
+    # appears in the header_links list
+    for text in links_to_assert:
+        assert text in header_links, f"'{text}' link is not displayed in the header menu"
+
+
+
+
+
+
+
+
+
+
